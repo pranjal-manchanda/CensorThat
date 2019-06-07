df832db75128c34bb41673425e9fc85505513af2
pranjal-manchanda <manchanda.pranjal@gmail.com>
2019-06-07 02:19:53 -0500

    Updated bleep.py for punctuations

--- bleep.py
+++ bleep.py
@@ -1,6 +1,6 @@
-from cs50 import get_string
 from sys import argv
 import sys
+
 """
 Program imports a list of banned words and ensures that they are censored out
 from any input text.
@@ -15,10 +15,9 @@ def main():
         sys.exit(1)
     # Function to load banned words into bannedWords set
     load()
-    # Get user input for a string they would like to censor
-    message = get_string("What message would you like to censor?\n")
-    # Remove punctuations and store message as a list of words
-    message = message.split()
+    # Get user input for a string they would like to censor and convert into
+    # list of words
+    message = input("What message would you like to censor? ").split()
     # Call function to censor message
     message = censor(message)
     # Convert message list into a string separated by spaces
@@ -36,14 +35,23 @@ def load():
 
 # Function to censor the words passed in as input string by user
 def censor(message):
-    # Iterate through each word in message list and check if the lowercase (case-insensitive)
-    # version of that exists in banned words
-    # If it does, replace the length of that word with that many *
-    # Return message
-    length = len(message)
-    for i in range(length):
-        if message[i].lower() in bannedWords:
-            message[i] = "*" * len(message[i])
+    
+    """ Iterate through each word in message list and check if the lowercase (case-insensitive)
+        version of that exists in banned words
+        If it does, replace the length of that word with that many *
+        Return message 
+    """
+    punctuation = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    for i in range(len(message)):
+        if message[i][-1] in punctuation: #If word ends in a punctuation
+            if message[i][:-1] in bannedWords: #If word is in bannedWords
+                #Replace word with *, keep punctuation
+                message[i] = ((len(message[i]) - 1) * "*") + (message[i][-1])
+        else:
+            #if word is in bannedWords
+            if message[i][:-1] in bannedWords:
+                message[i] = len(message[i]) * "*" #replace word with *
+    
     return message
 
 # Call main when the program starts
